82d26fa8bde6059eac468f8b6dbc3bbc31ea07c2
lokin-king <87030256+lokin-king@users.noreply.github.com>
2024-10-14 20:49:36 +0300

    Add files via upload

---
+++ lab_6_d_6.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>  
+
+using namespace std;
+
+int main() {
+    double x, y, x0, y0, R, distance;
+
+    cout << "M(x, y): ";
+    cin >> x >> y;
+
+    cout << "C(x0, y0) , R: ";
+    cin >> x0 >> y0 >> R;
+
+   
+    distance = sqrt(pow(x - x0, 2) + pow(y - y0, 2));
+
+    
+    if (distance <= R) {
+        cout << "Na koli" << endl;
+    }
+    else {
+        cout << "Ne na koli" << endl;
+    }
+
+    return 0;
+}
