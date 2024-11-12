a7513344463d3254767472c04faa4dc52a2665f1
lokin-king <87030256+lokin-king@users.noreply.github.com>
2024-11-12 11:51:35 +0200

    Update lab_8_d_6.cpp

--- lab_8_d_6.cpp
+++ lab_8_d_6.cpp
@@ -5,23 +5,22 @@ using namespace std;
 
 int main() {
     double a, b, c, x, y;
-    cout << "Введіть розміри цеглини (a, b, c): ";
+    cout << " (a, b, c): ";
     cin >> a >> b >> c;
-    cout << "Введіть розміри отвору (x, y): ";
+    cout << " (x, y): ";
     cin >> x >> y;
 
-    // Сортуємо розміри отвору і цеглини для зручності порівняння
+
     double brickSides[] = {a, b, c};
     double holeSides[] = {x, y};
 
-    sort(brickSides, brickSides + 3);  // сортуємо a, b, c
-    sort(holeSides, holeSides + 2);    // сортуємо x, y
+    sort(brickSides, brickSides + 3);  
+    sort(holeSides, holeSides + 2);    
 
-    // Перевіряємо, чи пройде найменша пара сторін цеглини через отвір
     if (brickSides[0] <= holeSides[0] && brickSides[1] <= holeSides[1]) {
-        cout << "Цеглина пройде через отвір." << endl;
+        cout << "prolize." << endl;
     } else {
-        cout << "Цеглина не пройде через отвір." << endl;
+        cout << "ne proliz." << endl;
     }
 
     return 0;
